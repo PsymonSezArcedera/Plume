11009a4d20b4337890858464bc520f7567862555
PsymonSezArcedera <pearcedera@up.edu.ph>
2025-01-13 17:22:21 +0800

    feat: ADD user and blog models

---
+++ backend/models/blogModel.js
@@ -0,0 +1,15 @@
+import mongoose from "mongoose";
+
+const blogSchema = new mongoose.Schema(
+    {
+        title: {type:String, required:true},
+        content: {type:String, required:true},
+        category: {type:String, required:true},
+        author: {type:String, required:true},
+        date: {type:Date, required:true},
+        rating: {type:Number, required:true}
+    },
+);
+
+const Blog = mongoose.model('Blog', blogSchema);
+export default Blog;
